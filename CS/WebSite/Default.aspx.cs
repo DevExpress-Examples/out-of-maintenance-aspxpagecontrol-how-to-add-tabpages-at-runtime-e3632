b7078a885e78215bc65907cb70cbc62d277310be
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxTabControl;
+using DevExpress.Web.ASPxGridView;
+using DevExpress.Web.ASPxEditors;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+        ASPxPageControl pageControl = new ASPxPageControl();
+        pageControl.ID = "pageControl";
+        Form.Controls.Add(pageControl);
+        pageControl.TabPages.Add("Page 1", "Page 1");
+        pageControl.TabPages.Add("Page 2", "Page 2");
+
+        ASPxGridView grid = new ASPxGridView();
+        grid.ID = "grid";
+        pageControl.TabPages[0].Controls.Add(grid);
+        grid.DataSource = AccessDataSource1;
+        grid.DataBind();
+
+        ASPxTextBox txt = new ASPxTextBox();
+        txt.ID = "txt";
+        txt.Text = "myTextBox";
+        pageControl.TabPages[1].Controls.Add(txt);
+
+
+
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxTabControl
+Imports DevExpress.Web.ASPxGridView
+Imports DevExpress.Web.ASPxEditors
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim pageControl As New ASPxPageControl()
+		pageControl.ID = "pageControl"
+		Form.Controls.Add(pageControl)
+		pageControl.TabPages.Add("Page 1", "Page 1")
+		pageControl.TabPages.Add("Page 2", "Page 2")
+
+		Dim grid As New ASPxGridView()
+		grid.ID = "grid"
+		pageControl.TabPages(0).Controls.Add(grid)
+		grid.DataSource = AccessDataSource1
+		grid.DataBind()
+
+		Dim txt As New ASPxTextBox()
+		txt.ID = "txt"
+		txt.Text = "myTextBox"
+		pageControl.TabPages(1).Controls.Add(txt)
+
+
+
+	End Sub
+End Class
